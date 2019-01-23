2b1260ed59117fa4745f6f140a43a756f9b8bd1e
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2019-01-23 19:39:18 +0100

    Use Autofac for dependency injection

--- Wator.Visualization/Program.cs
+++ Wator.Visualization/Program.cs
@@ -1,11 +1,36 @@
+using Autofac;
+using Wator.Simulation.Grid;
+
 namespace Wator.Visualization
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            var grid = new Simulation.TorusGrid(100, 100);
-            var simulation = new Simulation.Simulation(grid);
+            var container = BuildContainer();
+            RunSimulation(container);
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            const int gridWidth = 200;
+            const int gridHeight = 100;
+            builder.Register(c => new TorusGrid(gridWidth, gridHeight)).As<IGrid>();
+
+            builder.RegisterType<Simulation.Simulation>().As<Simulation.ISimulation>();
+
+            return builder.Build();
+        }
+
+        private static void RunSimulation(IContainer container)
+        {
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var simulation = scope.Resolve<Simulation.ISimulation>();
+                simulation.Step();
+            }
         }
     }
 }
